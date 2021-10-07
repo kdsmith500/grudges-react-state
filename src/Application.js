3e87530a7ea890c3c171e10226699d09a13147d0
Kyle Smith <k.d.smith500@gmail.com>
2021-10-07 14:31:58 -0500

    removes package-lock.json and generates yarn.lock, implements a reducer to abstract state away from the component, optimizes rendering of components with memo and useCallback

--- src/Application.js
+++ src/Application.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useReducer, useCallback } from 'react';
 
 import id from 'uuid/v4';
 
@@ -7,23 +7,47 @@ import NewGrudge from './NewGrudge';
 
 import initialState from './initialState';
 
+// saving strings as constants instead of just using strings will provide more useful errors
+const GRUDGE_ADD = 'GRUDGE_ADD';
+const GRUDGE_FORGIVE = 'GRUDGE_FORGIVE';
+
+// reducers are easy to unit test because they abstract state away from the component to the reducer
+const reducer = (state, action) => {
+  if (action.type === GRUDGE_ADD) {
+    return [action.payload, ...state];
+  }
+
+  if (action.type === GRUDGE_FORGIVE) {
+    return state.map(grudge => {
+      if (grudge.id !== action.payload.id) return grudge;
+      return { ...grudge, forgiven: !grudge.forgiven };
+    })
+  }
+
+  return state;
+}
+
 const Application = () => {
-  const [grudges, setGrudges] = useState(initialState);
+  const [grudges, dispatch] = useReducer(reducer, initialState);
 
-  const addGrudge = grudge => {
-    grudge.id = id();
-    grudge.forgiven = false;
-    setGrudges([grudge, ...grudges]);
-  };
+  const addGrudge = useCallback(({ person, reason }) => {
+    dispatch({
+      type: GRUDGE_ADD,
+      payload: {
+        person,
+        reason,
+        forgiven: false,
+        id: id()
+      }
+    });
+  }, [dispatch]);
 
-  const toggleForgiveness = id => {
-    setGrudges(
-      grudges.map(grudge => {
-        if (grudge.id !== id) return grudge;
-        return { ...grudge, forgiven: !grudge.forgiven };
-      })
-    );
-  };
+  const toggleForgiveness = useCallback(id => {
+    dispatch({
+      type: GRUDGE_FORGIVE,
+      payload: { id }
+    });
+  }, [dispatch]);
 
   return (
     <div className="Application">
